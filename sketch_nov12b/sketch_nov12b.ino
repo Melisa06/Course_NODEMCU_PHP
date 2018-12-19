b6d3bb28ede6bbfac39f28223a6e2094ebe45da0
Melisa06 <35781174+Melisa06@users.noreply.github.com>
2018-12-19 10:41:18 -0600

    next commit

--- sketch_nov12b/sketch_nov12b.ino
+++ sketch_nov12b/sketch_nov12b.ino
@@ -1,4 +1,4 @@
-#include <dummy.h>
+//#include <dummy.h>
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h> 
@@ -6,11 +6,11 @@
 #include <ESP8266HTTPClient.h>
 
 /* Set these to your desired credentials. */
-const char *ssid = "";  //ENTER YOUR WIFI SETTINGS
-const char *password = "";
+const char *ssid = "Intel-IoT";  //ENTER YOUR WIFI SETTINGS
+const char *password = "1N73RN37D3L45C0545";
 
 //Web/Server address to read/write from 
-const char *host = "";   //https://circuits4you.com website or IP address of server
+const char *host = "172.31.150.237";   //https://circuits4you.com website or IP address of server
 
 //=======================================================================
 //                    Power on setup
@@ -19,6 +19,8 @@ const char *host = "";   //https://circuits4you.com website or IP address of ser
 void setup() {
   delay(1000);
   Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, !LOW);
   WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
   delay(1000);
   WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
@@ -48,21 +50,22 @@ void loop() {
   HTTPClient http;    //Declare object of class HTTPClient
 
   String ADCData, temp, hum, mov, luz, postData;
-  int adcvalue=analogRead(A0);  //Read Analog value of LDR
+  int v_luz = (1024-analogRead(A0))/10.24;
+  luz = String(v_luz);
   //ADCData = String(842);   //String to interger conversion
   
   temp = "1";
   hum = "2";
   mov= "3";
-  luz ="4";
+  //luz ="4";
  
 
   //Post Data
-postData = "temp="  + temp  +  "&hum="  + hum +  "&mov="   + mov +  "&luz="  + luz;
+  postData = "temp="  + temp  +  "&hum="  + hum +  "&mov="   + mov +  "&luz="  + luz;
  //postData = "$temp=12,$hum=65,$mov=59,$lum=78";
   
-  http.begin("");              //Specify request destination
- http.addHeader("Content-Type", "application/x-www-form-urlencoded");    //Specify content-type header
+  http.begin("http://cidtai.uteq.edu.mx/intel/iot/variables/Pruebas1/Query/prueba1.php");              //Specify request destination
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");    //Specify content-type header
   //http.addHeader("Content-Type", "text/plain");    //Specify content-type header
    
   long httpCode = http.POST(postData);   //Send the request
@@ -73,8 +76,21 @@ postData = "temp="  + temp  +  "&hum="  + hum +  "&mov="   + mov +  "&luz="  + l
   Serial.println(postData);
 
   http.end();  //Close connection
+
+  //ACTUADORES
+  if(payload.indexOf("1") > 0)
+  {
+    digitalWrite(LED_BUILTIN, !HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, !LOW);
+  }
+
+  
   
   delay(5000);  //Post Data at every 5 seconds
 }
+
 
 
